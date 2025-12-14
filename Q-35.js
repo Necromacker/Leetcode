f5ff1216b63ea09e16ad5125fa51014a44ead41a
Krishna Gorde <krishnagorde04@gmail.com>
2025-12-14 23:09:16 +0530

    Update Q-35.js

--- Q-35.js
+++ Q-35.js
@@ -3,7 +3,12 @@
  * @param {number} target
  * @return {number}
  */
-
+var searchInsert = function (nums, target) {
+  for (i = 0; i < nums.length; i++) {
+    if (target > nums[i] && target <= nums[i + 1]) {
+      return i + 1;
+      break;
+    }
     if (nums[i] > target) {
       return i;
     }
